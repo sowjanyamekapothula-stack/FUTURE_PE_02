f9697847c6e62ab67601f1c6f3be30f465dbee0c
sowjanya <sowjanyamekapothula@gmail.com>
2026-04-16 23:19:58 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function showMessage() {
+    alert("Appointment booked! We will contact you soon.");
+}
+function showMessage() {
+    alert("Appointment booked! We will contact you soon.");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+
+    alert("Thank you " + name + "! Your appointment is booked.");
+}
